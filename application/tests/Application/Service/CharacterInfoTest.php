2746666a889fbb1a0257f0e2f538db399691673d
rolando-caldas <rolando.caldas@gmail.com>
2018-11-15 20:15:08 +0100

    files to CI/CD

--- application/tests/Application/Service/CharacterInfoTest.php
+++ application/tests/Application/Service/CharacterInfoTest.php
@@ -9,7 +9,24 @@
 namespace App\Tests\Application\Service;
 
 
-class CharacterInfoTest
+use App\Tests\Mock\CharacterRepository;
+use Application\Service\CharacterInfo;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class CharacterInfoTest extends TestCase
 {
 
+    private $characterRepository;
+
+    public function setUp()
+    {
+        $this->characterRepository = new CharacterRepository();
+    }
+
+    public function testHandle()
+    {
+        $applicationService = new CharacterInfo($this->characterRepository);
+        $character = $applicationService->handle(1);
+        $this->assertEquals($character->getId(), 1);
+    }
 }
--- application/tests/Mock/CharacterRepository.php
+++ application/tests/Mock/CharacterRepository.php
@@ -6,10 +6,66 @@
  * Time: 20:53
  */
 
-namespace App\Tests\Infrastructure;
+namespace App\Tests\Mock;
 
 
-class CharacterRepository
+use Domain\Entity\Character;
+use Domain\ValueObject\CharacterApiData;
+use Domain\ValueObject\Thumbnail;
+
+class CharacterRepository implements \Domain\Entity\CharacterRepository
 {
+    private $characters;
+
+    public function __construct()
+    {
+        $this->characters = [
+            1 => Character::createFromApiData(new CharacterApiData(
+                1,
+                'Character 1',
+                'Character 1 description',
+                new Thumbnail('file-path', 'jpg'))),
+        ];
+    }
+
+    public function save(Character $character): bool
+    {
+        $this->characters[$character->getId()] = $character;
+        return true;
+    }
+
+    public function saveCollection(Character ...$collection): bool
+    {
+        foreach ($collection AS $item) {
+            $this->save($item);
+        }
+        return true;
+    }
+
+    public function recoverErrors(bool $truncate): ?array
+    {
+        return null;
+    }
+
+    public function findCharacter(int $id): ?Character
+    {
+        return array_key_exists($id, $this->characters) ? $this->characters[$id] : null;
+    }
+
+    public function findCharactersById(int ...$collection): array
+    {
+        $return = [];
+        foreach ($collection AS $id) {
+            if (array_key_exists($id, $this->characters)) {
+                $return[] = $this->characters[$id];
+            }
+        }
+        return $return;
+    }
+
+    public function recoverAllCharacters(): array
+    {
+        return $this->characters;
+    }
 
 }
